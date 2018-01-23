12907300764832b9c4f41b25ecec99e1407e1f13
Vincent Rodier <vincent_top_cool@hotmail.com>
2018-01-23 10:58:35 -0500

    Ajout du centrage de la camera sur le la position de l'utilisateur lors de l'ouverture de la carte.

--- INF8405 - Informatique mobile/Wifi-Searcher/app/src/main/java/polytechnique/wifi_searcher/fragments/MapFrag.java
+++ INF8405 - Informatique mobile/Wifi-Searcher/app/src/main/java/polytechnique/wifi_searcher/fragments/MapFrag.java
@@ -2,6 +2,7 @@ package polytechnique.wifi_searcher.fragments;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -11,9 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import polytechnique.wifi_searcher.R;
 
@@ -24,25 +30,44 @@ import polytechnique.wifi_searcher.R;
 public class MapFrag extends Fragment{
 
     //SupportMapFragment mapFrag;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private GoogleMap mGoogleMap;
+
     MapView mMapView;
+    OnSuccessListener locationSuccess =  new OnSuccessListener<Location>() {
+        @Override
+        public void onSuccess(Location location) {
+            if (location != null) {
+                centerToLocation(location.getLatitude(), location.getLongitude());
+            }
+        }
+    };
 
     private OnMapReadyCallback onMapReadyCallback = new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap googleMap) {
             //TODO: Ajouter les marqueurs ici avec l'objet googleMap
+            mGoogleMap = googleMap;
             if ( ContextCompat.checkSelfPermission( getActivity(), Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
 
                 ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             }
-        googleMap.setMyLocationEnabled(true);
+            googleMap.setMyLocationEnabled(true);
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(locationSuccess);
         }
     };
+
+    private void centerToLocation(double lat, double lon){
+        LatLng latlong = new LatLng(lat, lon);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlong, 15));
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.map_layout, container, false);
         initializeView(view, savedInstanceState);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
         return view;
     }
 
