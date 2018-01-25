75cfc8ca56185d74381add7536739abef451b78b
Vincent Rodier <vincent.rodier@polymtl.ca>
2018-01-25 16:18:54 -0500

    Ajout de fichier non push

---
+++ INF8405 - Informatique mobile/Wifi-Searcher/app/src/main/java/polytechnique/wifi_searcher/activities/ViewBeaconActivity.java
@@ -0,0 +1,17 @@
+package polytechnique.wifi_searcher.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vincent on 2018-01-25.
+ */
+
+public class ViewBeaconActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
