2a79abc7b320d5f3687996bf864161ab8beb3964
Etienne <etass@DESKTOP-ERQ8PTB.localdomain>
2018-03-02 12:05:43 -0500

    ajout de partie B

---
+++ DB/DB_Project/DB_Project/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DB_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Thread-safe client factory
+            var redisManager = new PooledRedisClientManager("localhost:6379");
+
+            redisManager.ExecAs<Todo>(redisTodos => {
+                var todo = new Todo
+                {
+                    Id = redisTodos.GetNextSequence(),
+                    Content = "Learn Redis",
+                    Order = 1,
+                };
+
+
+                redisTodos.Store(todo);
+
+                Todo savedTodo = redisTodos.GetById(todo.Id);
+                savedTodo.Done = true;
+
+                redisTodos.Store(savedTodo);
+
+ //               redisTodos.DeleteById(savedTodo.Id);
+
+                var allTodos = redisTodos.GetAll();
+
+            });
+        }
+
+    }
+    }
